452ed8494a9d5b5af0d6b22fc5c9266418c3113b
danfunk <daniel.h.funk@gmail.com>
2023-08-26 11:15:49 -0400

    variabout improvements - cleaned up moviel view, added carousel, addied science section,

---
+++ static/js/carousel.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            700:{
+                items:2,
+                nav:false
+            },
+            1100:{
+                items:3,
+                nav:false
+            },
+        }
+    });
+});
